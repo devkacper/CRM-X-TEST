ff30416034327b86489606fa3ceb9b560ad04ce6
kacper <kacperlstasiak@gmail.com>
2021-11-29 22:47:01 +0100

    WIP: Added functionality of taking data from uploaded HTML file and store informations in database.

--- resources/views/modules/importer/index.blade.php
+++ resources/views/modules/importer/index.blade.php
@@ -1,4 +1,7 @@
 <form enctype="multipart/form-data"  action="/importers" method="POST">
-    <input type="file" name="file" accept=".html">
+    <input type="file" name="htmlFile" accept=".html">
     <input type="submit" value="Import & Store data">
+    @if($errors->any())
+        <span style="color: red; display: block">{{ $errors->first() }}</span>
+    @endif
 </form>
